c41f1a93bbaaf0dea30798b07810a52377ee43ef
Gurzafar Singh <159003771+Guri-Veera@users.noreply.github.com>
2024-07-17 23:29:02 +0530

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -7,7 +7,7 @@ function Home() {
   return (
     <div className='flex flex-col items-center p-16'>
       <h1 className='text-6xl text-black font-bold p-5'>Welcome to our Camera Shop!</h1>
-      <Link to='/shop' className='bg-yellow-600 p-2 rounded-lg mx-2 hover:scale-105 hover:bg-yellow-700 shadow-lg  m-16 py-2 px-4'>Go Straight to Shopping</Link>
+      <Link to='/shop' className='bg-[rgb(100,100,100)] p-2 rounded-lg mx-2 hover:scale-105 hover:bg-yellow-700 shadow-lg  m-16 py-2 px-4'>Go Straight to Shopping</Link>
     </div>
   )
 }
